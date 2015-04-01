735b5312761a674db359326320b4e6ab05337db3
dkamas273 <dkamas273@PR28.hhs.win.fuhsd.org>
2015-04-01 12:57:27 -0700

    commit 4.5.2

--- NumberLine/src/ControlListener.java
+++ NumberLine/src/ControlListener.java
@@ -1,11 +1,13 @@
 
 public interface ControlListener {
+
+	public static final int CIRCLE = 0;
+	public static final int SQUARE = 1;
+	public static final int TRIANGLE = 2;
 	
-	public static final int SQUARE = 0;
-	public static final int TRIANGLE = 1;
-	public static final int CIRCLE = 2;
-	
-	void changeShape(int shape);
-	void moveSlider(int num);
 	void setFont(boolean bold, boolean italics, boolean underlined);
+	void setShape(int shape);
+	void setNumber(double num);
+	
+	
 }
--- NumberLine/src/DrawingPanel.java
+++ NumberLine/src/DrawingPanel.java
@@ -1,21 +1,30 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.font.TextAttribute;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class DrawingPanel extends JPanel{
+public class DrawingPanel extends JPanel implements ControlListener {
 	// TODO Your Instance Variables Here
 
-	final int absoluteHeight, absoluteWidth;
+	private final int absoluteWidth;
+	private Font font;
+	private double number;
+	private int myShape;
 	
 	public DrawingPanel() {
 		super();
 		setBackground(Color.WHITE);
-		absoluteHeight = getHeight();
-		absoluteWidth = getWidth();
-		// TODO Add GUI customizations to the panel
+		absoluteWidth = 640;
+		font = new Font("Sans Serif",Font.BOLD,16);
+		number = 50.0;
+		myShape = ControlListener.CIRCLE;
 	}
 
 	public void paintComponent(Graphics g) {
@@ -23,11 +32,61 @@ public class DrawingPanel extends JPanel{
 									// to paint the background
 
 		int width = getWidth();
-		int height = getHeight();
+		double scale = (double)width/absoluteWidth;
 		
-		g.drawLine()
+		g.setColor(Color.CYAN);
+		g.fillRect((int)(64 * scale), 95, (int)((absoluteWidth - 128)*scale), 10);
+		g.setColor(Color.BLACK);
+		g.drawLine((int)(64 * scale), 100, (int)((absoluteWidth - 64)*scale), 100);
+		g.setFont(font);
+		FontMetrics fm = g.getFontMetrics();
+		
+		for (int i = 0; i <= 10; i++) {
+			g.drawLine((int)((64 + 51.2*i) * scale), 90, (int)((64 + 51.2*i) * scale), 110);
+			width = fm.stringWidth(i*10 + "");
+			g.drawString(i*10 + "", (int)((64 + 51.2*i) * scale) - width/2, 70);
+		}
+		
+		g.setColor(Color.RED);
+		if (myShape == ControlListener.CIRCLE)
+			g.fillOval((int)((64 + 5.12*number - 5) * scale) - 5 + (int)(5 * scale), 95, 10, 10);
+		if (myShape == ControlListener.TRIANGLE)
+			g.fillPolygon(new int[]{(int)((64 + 5.12*number - 5) * scale) - 5 + (int)(5 * scale), (int)((64 + 5.12*number - 5) * scale) + (int)(5 * scale), (int)((64 + 5.12*number - 5) * scale) + 5 + (int)(5 * scale)}, new int[]{105,95,105}, 3);
+		if (myShape == ControlListener.SQUARE)
+			g.fillRect((int)((64 + 5.12*number - 5) * scale) - 5 + (int)(5 * scale), 95, 10, 10);
 		
 		// TODO Draw on the panel here
 	}
+	
+	@Override
+	public void setFont(boolean bold, boolean italics, boolean underlined) {
+		int add = 0;
+		if (bold) {
+			add += Font.BOLD;
+		}
+		if (italics) {
+			add += Font.ITALIC;
+		}
+		font = new Font("Sans Serif", add, 16);
+		if (underlined) {
+			Map<TextAttribute, Object> map = new Hashtable<TextAttribute, Object>();
+			map.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+			font = font.deriveFont(map);
+		}
+		repaint();
+	}
 
+	@Override
+	public void setShape(int shape) {
+		myShape = shape;
+		repaint();
+	}
+
+	@Override
+	public void setNumber(double num) {
+		number = num;
+		repaint();
+	}
+
+	
 }
